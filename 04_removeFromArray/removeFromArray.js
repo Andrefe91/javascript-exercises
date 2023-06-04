436ad9f9d09c2c1b617b9d877b479d8e77f04a42
Andres Felipe Munoz <109099541+Andrefe91@users.noreply.github.com>
2023-06-04 20:29:06 +0000

    Adds the solution to the fourth excersice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...restArguments) {
+
+    newArray =[];
+    deleteIndices = [];
+
+    /*We must first find de indices to delete within the array*/
+    for (let i in restArguments) {
+        for (let j in array){
+            if (restArguments[i] === array[j]){
+                deleteIndices.push(j);
+            }
+        }
+    }
+
+    /*We need to sort the indices*/
+    deleteIndices.sort();
+
+    /* Now, instead of delete entries in the old array, we populate a new one*/
+    outer:for (let i in array){
+        for (let j of deleteIndices) {
+            if (i == j){
+                continue outer;
+            } 
+        }
+        newArray.push(array[i]);
+    }
+
+    return newArray;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
