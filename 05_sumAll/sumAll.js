128747d73486c107ac54e5143cf8d49d6bd5c7dc
Andres Felipe Munoz <109099541+Andrefe91@users.noreply.github.com>
2023-06-05 03:15:47 +0000

    Adds the solution for the fifth excersice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
 
+    if (typeof numberOne !== 'number' || typeof numberTwo !== 'number' || numberOne < 0 || numberTwo < 0){
+        return 'ERROR'
+
+    } else {
+
+        if (numberOne > numberTwo){
+            min = numberTwo;
+            max = numberOne;
+        }else {
+            max = numberTwo;
+            min = numberOne;
+        }
+    
+        total = 0;
+        for (min; min<= max; min++){
+            total += min;
+        }
+    
+        return total;
+
+    }
 };
 
+sumAll(1,10);
 // Do not edit below this line
 module.exports = sumAll;
