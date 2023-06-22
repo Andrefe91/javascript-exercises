9f493605b0679db510afd0fd4c8745c968600b5d
Andres Felipe -Main <andrefe91@gmail.com>
2023-06-21 23:14:11 -0500

    Adds the solution to the eight excersice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum,a) => sum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply,a) => multiply * a, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = a;
+	for (let i=1; i<b; i++) {
+    result *= a;
+  };
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+
+  if (a == 0) {
+    result = 1;
+  }else {
+    for (let i=a-1; i>=1; i--) {
+      result *= i;
+    };
+  }
+
+  return result;
 };
 
 // Do not edit below this line
