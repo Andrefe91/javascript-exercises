a029b9a8a603c2a32d5c81cf98445ce3e578d0bf
Andres Felipe -Main <andrefe91@gmail.com>
2023-06-22 23:06:37 -0500

    Adds the solution to the twelve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,84 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    let currentYear = (new Date).getFullYear();
+
+    function sortPersons(a,b){
+
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = currentYear;
+        } else if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = currentYear;
+        }
+
+        let ageLastPerson = a.yearOfDeath - a.yearOfBirth;
+        let ageNextPerson = b.yearOfDeath - b.yearOfBirth;
+
+        if (ageLastPerson < ageNextPerson) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    persons.sort(sortPersons);
+    return persons[0];
+    
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+
+const people2 = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+const people3 = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
+
+console.log(findTheOldest(people2));
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
