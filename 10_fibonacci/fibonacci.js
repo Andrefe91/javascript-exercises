cb68bbaa7892f5964e93bf5e76a61c9a00377f9f
Andres Felipe -Main <andrefe91@gmail.com>
2023-06-22 22:13:52 -0500

    Adds the solution to the tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+    if (position < 0) {
+        return 'OOPS';
+    }
+
+    /*First, define a starting array and variable for loops*/
+    let numberLoops = position -2; 
+    let sequence = [1,1];
+
+    /*Now, construct the Sequence with a loop*/
+    for (let i = numberLoops; i > 0; i--) {
+        
+        let arrayLength = sequence.length -1;
+
+        sequence.push(sequence[arrayLength]+sequence[arrayLength-1]);
+    }
+    
+    return sequence[position-1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
